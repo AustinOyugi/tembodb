28faddfa560d70600fee7254a049412dc868e5ef
Austin Oyugi <austinoyugi@gmail.com>
2026-03-26 14:25:19 +0300

    added bytemuck, and util functions for tembo page

--- src/storage/layout/tembo_page.rs
+++ src/storage/layout/tembo_page.rs
@@ -1,6 +1,63 @@
-const PAGE_SIZE: usize = 8192;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+use bytemuck::{Pod, Zeroable};
+
+const PAGE_SIZE: u16 = 8192;
 
 #[repr(C)]
+#[derive(Debug)]
+pub struct TemboPage {
+    data: [u8; PAGE_SIZE as usize],
+}
+
+
+// Total 4 + 2 + 2 + 2 + 2 =  12 bytes
+#[repr(C)]
+#[derive(Debug,Copy, Clone, Pod, Zeroable)]
+pub struct TemboPageHeader {
+    // Unique identifier for the page
+    id: u32,
+
+    // Stores the number of slots available in the page
+    slot_count: u16,
+
+    // Tells where the next slot should be allocated
+    free_start: u16,
+
+    // Tells where the record should be written at, from the end
+    free_end: u16,
+
+    // we are aligning in terms of 4 bytes u32
+    _pad: u16
+}
+
+impl TemboPageHeader {
+    pub fn root(id: u32) -> Self {
+        TemboPageHeader {
+            id,
+            slot_count: 0,
+            free_start: size_of::<TemboPageHeader>() as u16,
+            free_end: PAGE_SIZE,
+            _pad: 0,
+        }
+    }
+}
+
+
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct LinePointer {
+    // Where the record starts inside the page
+    offset: u16,
+    // The size of the tuple
+    length: u16,
+}
+
+
+/// The first page that gives metadata about the pages
+#[repr(C)]
+#[derive(Copy, Clone, Pod, Zeroable, Debug)]
 pub struct TemboPageZero {
 
     // supposed to track the page serial version, e.g b"Tembo\0\0\0"
@@ -21,49 +78,26 @@ pub struct TemboPageZero {
     // the id of the first page
     root_page: u32,
 
+
     // Total size
     // 8 + 2 + 2 + 4 + 4 =  20 bytes
     // 8192 - 20  = 8172 bytes left
     reserved: [u8; 8172],
 }
 
-#[repr(C)]
-#[derive(Debug)]
-pub struct TemboPageHeader {
-    // Unique identifier for the page
-    id: u32,
+const TEMBO_MAGIC: [u8;8] = *b"TEMBO\0\0\0";
 
-    // Stores the number of slots available in the page
-    slot_count: u16,
-
-    // Tells where the next slot should be allocated
-    free_start: u16,
-
-    // Tells where the record should be written at, from the end
-    free_end: u16,
-
-    // we are aligning in terms of 4 bytes u32
-    _pad: u16
-}
-
-/// Total size
-/// 4 + 1 = 5
-/// padding required is 3
-///  Total is 8 bytes
-#[repr(C)]
-#[derive(Debug)]
-pub struct LinePointer {
-    // Distance from the start where the tuple is stored
-    offset: u8,
-    // The size of the tuple
-    length: u32,
-    _pad: [u8; 3],
-}
-
-#[repr(C)]
-#[derive(Debug)]
-pub struct TemboPage {
-    data: [u8; PAGE_SIZE],
+impl TemboPageZero {
+    pub fn new() -> TemboPageZero {
+        TemboPageZero {
+            magic: TEMBO_MAGIC,
+            version: 0,
+            page_size: PAGE_SIZE,
+            total_pages: 1,
+            root_page: 1,
+            reserved: [0u8;8172],
+        }
+    }
 }
 
 
@@ -71,3 +105,53 @@ pub struct BufferPool {
     cache: Vec<[u8; 8192]>,
     dirty_pages: Vec<u32>,
 }
+
+
+fn get_page_zero_bytes() -> [u8; PAGE_SIZE as usize] {
+    let tembo_page_zero = &TemboPageZero::new();
+    const _: () = assert!(size_of::<TemboPageZero>() == 8192);
+    let page_zero_bytes = bytemuck::bytes_of(tembo_page_zero);
+    let mut zero_raw_page_buffer = [0u8;PAGE_SIZE as usize];
+    zero_raw_page_buffer[..size_of::<TemboPageZero>()].copy_from_slice(page_zero_bytes);
+    zero_raw_page_buffer
+}
+
+
+fn get_tembo_header_bytes() -> [u8; PAGE_SIZE as usize] {
+    let tembo_page_header = &TemboPageHeader::root(0);
+    let header_page_bytes = bytemuck::bytes_of(tembo_page_header);
+    let mut raw_page_buffer = [0u8; PAGE_SIZE as usize];
+    raw_page_buffer[..size_of::<TemboPageHeader>()].copy_from_slice(header_page_bytes);
+    raw_page_buffer
+}
+
+
+fn read_page_zero_from_file(file: &mut File) -> std::io::Result<TemboPageZero> {
+    let read_file = read_from_file(file, 0)?;
+    let read_page_zero: &TemboPageZero = bytemuck::from_bytes(&read_file);
+    Ok(read_page_zero.clone())
+}
+
+fn read_tembo_header(file: &mut File) -> std::io::Result<TemboPageHeader> {
+    let read_file = read_from_file(file, (PAGE_SIZE as u64) -1)?;
+    let header_section = read_file[..size_of::<TemboPageHeader>()].to_vec();
+    let read_page_header: &TemboPageHeader = bytemuck::from_bytes(&header_section);
+    Ok(read_page_header.clone())
+}
+
+fn read_from_file(file: &mut File, offset: u64) -> std::io::Result<[u8; PAGE_SIZE as usize]> {
+    let mut buffer = [0u8; PAGE_SIZE as usize];
+    file.seek(SeekFrom::Start(offset))?;
+    file.read_exact(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn write_to_file(file: &mut File, data: &[u8; PAGE_SIZE as usize], offset: u64) -> std::io::Result<()> {
+    file.seek(SeekFrom::Start(offset))?;
+    file.write_all(data)?;
+    file.flush()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tembo_page_tests;
---
+++ src/storage/layout/tembo_page/tembo_page_tests.rs
@@ -0,0 +1,49 @@
+use std::fs::OpenOptions;
+use super::*;
+
+#[test]
+fn test_movements() {
+
+    let file = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .read(true)
+        .open("tembo.db");
+
+    assert!(file.is_ok());
+    println!("File created!");
+
+    let mut file = file.unwrap();
+
+    let page_zero_bytes = get_page_zero_bytes();
+    let write_page_zero = write_to_file(&mut file, &page_zero_bytes, 0);
+    assert!(write_page_zero.is_ok());
+    println!("Write page zero!");
+
+    let get_tembo_header_bytes = get_tembo_header_bytes();
+    println!("Size of tembo header: {}", get_tembo_header_bytes.len());
+    let write_tembo_header = write_to_file(&mut file, &get_tembo_header_bytes, (PAGE_SIZE as u64 ) - 1);
+    assert!(write_tembo_header.is_ok());
+    println!("Write page header!");
+
+
+    let page_zero =  read_page_zero_from_file(&mut file);
+    assert!(page_zero.is_ok());
+    let page_zero = page_zero.unwrap();
+    assert_eq!(page_zero.page_size, PAGE_SIZE);
+    assert_eq!(page_zero.total_pages, 1);
+    assert_eq!(page_zero.root_page, 1);
+    assert_eq!(page_zero.magic, TEMBO_MAGIC);
+    assert_eq!(page_zero.version, 0);
+    println!("Read page zero!");
+
+
+    let tembo_page_header = read_tembo_header(&mut file);
+    assert!(tembo_page_header.is_ok());
+    let tembo_page_header = tembo_page_header.unwrap();
+    assert_eq!(tembo_page_header.id, 0);
+    assert_eq!(tembo_page_header.slot_count, 0);
+    assert_eq!(tembo_page_header.free_start, size_of::<TemboPageHeader>() as u16);
+    assert_eq!(tembo_page_header.free_end, PAGE_SIZE);
+}
